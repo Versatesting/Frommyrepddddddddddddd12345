d848a83d85bf09aecd332c1ab8719a206b2aac2d
Versatester123 <versatest123@gmail.com>
2026-05-14 21:40:31 +0900

    Update run.py

--- run.py
+++ run.py
@@ -1,1 +1,3 @@
 print("create is not working")
+https://github.com/users/Versatesting/projects/15/settingshttps://github.com/users/Versatesting/projects/15/settings
+https://github.com/users/Versatesting/projects/15/settings
